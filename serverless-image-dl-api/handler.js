c519d9ddb5ba0810575c7ccce4b35470440dbace
t-mine <tomonori.mine.biz@gmail.com>
2021-11-11 15:42:49 +0900

    Serverless Frameworkで画像をダウンロードするAPIを作成

---
+++ serverless-image-dl-api/handler.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const S3 = require('aws-sdk/clients/s3');
+const s3 = new S3({
+  apiVersion: '2006-03-01',
+  region: 'ap-northeast-1',
+});
+
+module.exports.getImage = async () => {
+  const params = {
+    Bucket: 'test-bucket',
+    Key: 'test.png',
+  };
+  const data = await s3.getObject(params).promise();
+  return data.Body.toString('base64');
+};
